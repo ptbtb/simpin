9628a777da046a2af0712e6ab4af119f740440d9
Arya Edogawa <130411100077@student.trunojoyo.ac.id>
2020-09-20 07:25:17 +0700

    Add error handler to login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,13 +31,16 @@ Login
 						<input class="input100" type="password" name="password" placeholder="Type your password">
 						<span class="focus-input100" data-symbol="&#xf190;"></span>
 					</div>
-					
+					@if(!empty($errors->first()))
+						<div class="">
+							<strong style="color: red; font-size: 11px">{{ $errors->first() }}</strong>
+						</div>
+					@endif
 					<div class="text-right p-t-8 p-b-31">
 						<a href="{{ url('/password/reset') }}">
 							Forgot password?
 						</a>
 					</div>
-					
 					<div class="container-login100-form-btn">
 						<div class="wrap-login100-form-btn">
 							<div class="login100-form-bgbtn"></div>
