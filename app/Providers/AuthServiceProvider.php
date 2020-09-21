b7888d6eeb3b9c372dedf03ebe35a911f52c78dc
Arya Edogawa <130411100077@student.trunojoyo.ac.id>
2020-09-21 07:45:19 +0700

    Update permission admin logic

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,7 +27,7 @@ class AuthServiceProvider extends ServiceProvider
 
         // give admin access to all permission
         Gate::before(function ($user, $ability) {
-            return $user->hasRole('admin') ? true : null;
+            return $user->hasRole('admin') || $user->hasRole('Admin') ? true : null;
         });
     }
 }
