f4828c692028c073f5154d67ec7074c04afebb06
Arya Edogawa <130411100077@student.trunojoyo.ac.id>
2020-11-30 07:54:08 +0700

    Fix typo

--- resources/views/simpanan/history.blade.php
+++ resources/views/simpanan/history.blade.php
@@ -11,7 +11,7 @@
 		<ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
             <li class="breadcrumb-item"><a href="">Pinjaman</a></li>
-			<li class="breadcrumb-item active">History Pinjaman</li>
+			<li class="breadcrumb-item active">History Simpanan</li>
 		</ol>
 	</div>
 </div>
