cf814bb7c9d9e8e9d04fc96605af8bedb837fc79
Arya Edogawa <130411100077@student.trunojoyo.ac.id>
2020-12-30 16:42:05 +0700

    Fix bug route

--- routes/web.php
+++ routes/web.php
@@ -164,7 +164,7 @@ Route::group(['prefix' => 'pinjaman'], function ()
         Route::post('list', [App\Http\Controllers\PinjamanController::class, 'index'])->name('pinjaman-list');
         Route::get('history', [App\Http\Controllers\PinjamanController::class, 'history'])->name('pinjaman-history');
         Route::post('history', [App\Http\Controllers\PinjamanController::class, 'history'])->name('pinjaman-history');
-        Route::get('detail/{id}', [App\Http\Controllers\PinjamanController::class, 'show'])->where('id', '[0-9]+')->name('pinjaman-detail');
+        Route::get('detail/{id}', [App\Http\Controllers\PinjamanController::class, 'show'])->name('pinjaman-detail');
         Route::get('download/pdf', [App\Http\Controllers\PinjamanController::class, 'createPDF'])->name('pinjaman-download-pdf');
         Route::get('download/excel', [App\Http\Controllers\PinjamanController::class, 'createExcel'])->name('pinjaman-download-excel');
         Route::get('download-form-pinjaman', [App\Http\Controllers\PinjamanController::class, 'downloadFormPinjaman'])->name('download-form-pinjaman');
