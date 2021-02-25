621528db0952c539ca750aca366b587d8f65c7a6
Satria Persada <triasada@yahoo.com>
2021-02-25 17:52:04 +0700

    import anggota password

--- app/Imports/AnggotaImport.php
+++ app/Imports/AnggotaImport.php
@@ -63,7 +63,7 @@ class AnggotaImport implements OnEachRow
         }
 
         $anggota = Anggota::create($fields);
-
-        event(new AnggotaCreated($anggota));
+        $password =  Hash::make($request->password);
+        event(new AnggotaCreated($anggota,$password));
     }
 }
