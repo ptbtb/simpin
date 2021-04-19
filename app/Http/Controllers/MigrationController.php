936238e27cc6b0d672acfc05c9737db5ed7b6100
Rivaldy Septyasurya <rivaldy.septyasurya@gmail.com>
2021-04-19 11:39:16 +0700

    Update code migration jurnal transaction

--- app/Http/Controllers/MigrationController.php
+++ app/Http/Controllers/MigrationController.php
@@ -39,14 +39,14 @@ class MigrationController extends Controller
         {
             // bulan its 1 = january, 2 = feb, 3 = maret, 1 running for 1 month choosed
 
-            $jurnals = JurnalTemp::whereMonth('tgl_posting', '=', $bulan)->get()->unique('no_bukti');
-
+            $jurnals = JurnalTemp::whereMonth('tgl_posting', '=', $bulan)->where('is_success', 0)->get()->unique('no_bukti');
+           
             $jenisSimpanan = JenisSimpanan::get();
             $jenisPinjaman = JenisPinjaman::get();
 
             foreach ($jurnals as $key => $jurnal) 
             {
-                $transactions = JurnalTemp::where('no_bukti', $jurnal->no_bukti)->get();
+                $transactions = JurnalTemp::where('no_bukti', $jurnal->no_bukti)->whereMonth('tgl_posting', '=', $bulan)->where('is_success', 0)->get();
 
                 // group by uraian3 because 1 kode_bukti has more than 1 transaction
                 $groupByUraian = $transactions->groupBy('uraian_3');
@@ -62,7 +62,69 @@ class MigrationController extends Controller
 
                     if(count($groupByAnggota) == 1)
                     {
-                        echo('NO ANGGOTA KOSONG : ' . $jurnal->no_bukti . "<br>");
+                        // simpanan
+                        if(count($uraian->where('normal_balance', 2)->whereIn('code', [40407000, 40401000, 40402000, 40404000, 40408000])) > 0 )
+                        {
+                            $transaction = $uraian->whereIn('code', [41101000, 41112000, 50201000, 40901000, 40903000])->first();
+
+                            if($transaction)
+                            {
+                                $newCode = substr($transaction->code,0,3).'.'.substr($transaction->code,3,2).'.'.substr($transaction->code,5,3);
+
+                                // get next serial number
+                                $nextSerialNumber = SimpananManager::getSerialNumber(Carbon::now()->format('d-m-Y'));
+
+                                $transactionDebet = $uraian->where('kode_anggota', 0)->where('normal_balance', 1)->first();
+
+                                if(!$transactionDebet)
+                                {
+                                    $transactionDebet = $uraian->where('normal_balance', 1)->first();
+                                }
+
+                                $newCodeDebet = substr($transactionDebet->code,0,3).'.'.substr($transactionDebet->code,3,2).'.'.substr($transactionDebet->code,5,3);
+
+                                $codeDebet = Code::where('CODE', $newCodeDebet)->first();
+
+                                $totalTransaction = $uraian->where('normal_balance', 2)->whereIn('code', [40407000, 40401000, 40402000, 40404000, 40408000])->sum('jumlah');
+
+                                $simpanan = new Simpanan();
+                                $simpanan->jenis_simpan = strtoupper($jenisSimpanan->where('kode_jenis_simpan', $newCode)->first()->nama_simpanan);
+                                $simpanan->besar_simpanan = $totalTransaction;
+                                $simpanan->kode_anggota = $transaction->kode_anggota;
+                                $simpanan->u_entry = 'Admin BTB';
+                                $simpanan->tgl_entri = Carbon::now();
+                                $simpanan->periode = $transaction->tgl_posting;
+                                $simpanan->kode_jenis_simpan = $jenisSimpanan->where('kode_jenis_simpan', $newCode)->first()->kode_jenis_simpan;
+                                $simpanan->keterangan = $transaction->uraian_3;
+                                $simpanan->id_akun_debet = $codeDebet->id;
+                                $simpanan->serial_number = $nextSerialNumber;
+                                $simpanan->save();
+
+                                echo('NO BUKTI SIMPANAN SUCCESS : ' . $jurnal->no_bukti . "<br>");
+
+                                $transactionSuccess = $simpanan;
+
+                                $idTipeJurnal = TIPE_JURNAL_JKM;
+
+                                foreach($uraian as $jurnalTemp)
+                                {
+                                    // update status jurnal_temp
+                                    $jurnalTemp->is_success = 1;
+                                    $jurnalTemp->save();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // save into jurnal because this transaction is buffer transaction, and change to success
+                            foreach($uraian as $jurnalTemp)
+                            {
+                                // update status jurnal_temp
+                                $jurnalTemp->is_success = 1;
+                                $jurnalTemp->keterangan_gagal = 'ANGGOTA KOSONG';
+                                $jurnalTemp->save();
+                            }
+                        }
                     }
                     else
                     {
@@ -108,6 +170,13 @@ class MigrationController extends Controller
                             $transactionSuccess = $simpanan;
 
                             $idTipeJurnal = TIPE_JURNAL_JKM;
+
+                            foreach($uraian as $jurnalTemp)
+                            {
+                                // update status jurnal_temp
+                                $jurnalTemp->is_success = 1;
+                                $jurnalTemp->save();
+                            }
                         }
                         // if penarikan
                         else if($jenisSimpanan->where('kode_jenis_simpan', $newCode)->first() && $transaction->normal_balance == 1)
@@ -141,14 +210,35 @@ class MigrationController extends Controller
                                     echo('NO BUKTI PENARIKAN SUCCESS : ' . $jurnal->no_bukti . "<br>");
 
                                     $transactionSuccess = $penarikan;
+
+                                    foreach($uraian as $jurnalTemp)
+                                    {
+                                        // update status jurnal_temp
+                                        $jurnalTemp->is_success = 1;
+                                        $jurnalTemp->save();
+                                    }
                                 }
                                 else
                                 {
                                     echo('NO BUKTI PENARIKAN, TABUNGAN KOSONG : ' . $jurnal->no_bukti . "<br>");
+
+                                    foreach($uraian as $jurnalTemp)
+                                    {
+                                        // update status jurnal_temp
+                                        $jurnalTemp->keterangan_gagal = 'NO BUKTI PENARIKAN, TABUNGAN KOSONG';
+                                        $jurnalTemp->save();
+                                    }
                                 }
                             }
                             else
                             {
+                                foreach($uraian as $jurnalTemp)
+                                {
+                                    // update status jurnal_temp
+                                    $jurnalTemp->keterangan_gagal = 'NO BUKTI PENARIKAN, TABUNGAN KOSONG';
+                                    $jurnalTemp->save();
+                                }
+
                                 echo('NO BUKTI PENARIKAN, TABUNGAN KOSONG : ' . $jurnal->no_bukti . "<br>");
                             }
 
@@ -213,6 +303,13 @@ class MigrationController extends Controller
                             $transactionSuccess = $pinjaman;
 
                             $idTipeJurnal = TIPE_JURNAL_JKK;
+
+                            foreach($uraian as $jurnalTemp)
+                            {
+                                // update status jurnal_temp
+                                $jurnalTemp->is_success = 1;
+                                $jurnalTemp->save();
+                            }
                         }
                         // angsuran
                         else if($jenisPinjaman->where('kode_jenis_pinjam', $newCode)->first() && $transaction->normal_balance == 2)
@@ -223,18 +320,19 @@ class MigrationController extends Controller
                             $kodeAnggota = $transaction->kode_anggota;
 
                             $pinjaman = Pinjaman::where('kode_anggota', $kodeAnggota)->where('kode_jenis_pinjam', $newCode)->first();
-                            
+
                             if($pinjaman)
                             {
                                 $jatuhTempo = $pinjaman->tgl_entri->addMonths(1)->endOfMonth();
 
-                                $sisaPinjaman = $pinjaman->besar_pinjam;
-                                $sisaPinjaman = $sisaPinjaman-$pinjaman->besar_angsuran_pokok;
+                                // count sisa pinjaman
+                                $besarPinjaman = $pinjaman->besar_pinjam;
+                                $sisaPinjaman = $besarPinjaman-$transaction->jumlah;
 
                                 $angsuran = new Angsuran();
                                 $angsuran->kode_pinjam = $pinjaman->kode_pinjam;
                                 $angsuran->angsuran_ke = 1;
-                                $angsuran->besar_angsuran = $transaction->jumlah;
+                                $angsuran->besar_angsuran = $pinjaman->besar_angsuran_pokok;
                                 $angsuran->denda = 0;
                                 $angsuran->jasa = $pinjaman->biaya_jasa;
                                 $angsuran->kode_anggota = $pinjaman->kode_anggota;
@@ -243,14 +341,42 @@ class MigrationController extends Controller
                                 $angsuran->jatuh_tempo = $jatuhTempo;
                                 $angsuran->u_entry = 'Administrator';
                                 $angsuran->serial_number = $nextSerialNumber;
+                                $angsuran->besar_pembayaran = $transaction->jumlah + $pinjaman->biaya_jasa;
+
+                                if( ($pinjaman->besar_pinjam - $transaction->jumlah) < 1)
+                                {
+                                    $angsuran->id_status_angsuran = 2;
+                                }
+                                else
+                                {
+                                    $angsuran->id_status_angsuran = 1;
+                                }
                                 $angsuran->save();
+
+                                // update sisa pinjaman
+                                $pinjaman->sisa_pinjaman -= $transaction->jumlah;
+                                $pinjaman->save();
 
                                 echo('NO BUKTI PINJAMAN SUCCESS : ' . $jurnal->no_bukti . "<br>");
 
                                 $transactionSuccess = $angsuran;
+
+                                foreach($uraian as $jurnalTemp)
+                                {
+                                    // update status jurnal_temp
+                                    $jurnalTemp->is_success = 1;
+                                    $jurnalTemp->save();
+                                }
                             }
                             else
                             {
+                                foreach($uraian as $jurnalTemp)
+                                {
+                                    // update status jurnal_temp
+                                    $jurnalTemp->keterangan_gagal = 'NO BUKTI ANGSURAN, PINJAMAN KOSONG';
+                                    $jurnalTemp->save();
+                                }
+
                                 echo('NO BUKTI ANGSURAN, PINJAMAN KOSONG : ' . $jurnal->no_bukti . "<br>");
                             }
                             
@@ -261,50 +387,53 @@ class MigrationController extends Controller
                     // save every jurnal to jurnal table
                     foreach ($uraian as $key => $uraianJurnal) 
                     {
-                        // save into jurnal table
-                        $newJurnal = new Jurnal();
-                        $newJurnal->id_tipe_jurnal = $idTipeJurnal;
-                        $newJurnal->nomer = Carbon::now()->format('Ymd').(Jurnal::count()+1);
-
-                        // new format for code
-                        $newCoa = substr($uraianJurnal->code,0,3).'.'.substr($uraianJurnal->code,3,2).'.'.substr($uraianJurnal->code,5,3);
-
-                        // debet
-                        if($uraianJurnal->normal_balance == 1)
+                        if($uraianJurnal->is_success == 1)
                         {
-                            $newJurnal->akun_debet = $newCoa;
-                            $newJurnal->debet = $uraianJurnal->jumlah;
-                            $newJurnal->akun_kredit = 0;
-                            $newJurnal->kredit = 0;
-                        }
-                        // kredit
-                        else
-                        {
-                            $newJurnal->akun_debet = 0;
-                            $newJurnal->debet = 0;
-                            $newJurnal->akun_kredit = $newCoa;
-                            $newJurnal->kredit = $uraianJurnal->jumlah;
-                        }
+                            // save into jurnal table
+                            $newJurnal = new Jurnal();
+                            $newJurnal->id_tipe_jurnal = $idTipeJurnal;
+                            $newJurnal->nomer = Carbon::now()->format('Ymd').(Jurnal::count()+1);
 
-                        $newJurnal->keterangan = $uraianJurnal->uraian_3;
-                        
-                        if($newJurnal->keterangan == '' || $newJurnal->keterangan == null)
-                        {
-                            $newJurnal->keterangan = '-';
-                        }
+                            // new format for code
+                            $newCoa = substr($uraianJurnal->code,0,3).'.'.substr($uraianJurnal->code,3,2).'.'.substr($uraianJurnal->code,5,3);
 
-                        $newJurnal->created_by = 1;
-                        $newJurnal->updated_by = 1;
-                        $newJurnal->created_at = $uraianJurnal->tgl_posting;
+                            // debet
+                            if($uraianJurnal->normal_balance == 1)
+                            {
+                                $newJurnal->akun_debet = $newCoa;
+                                $newJurnal->debet = $uraianJurnal->jumlah;
+                                $newJurnal->akun_kredit = 0;
+                                $newJurnal->kredit = 0;
+                            }
+                            // kredit
+                            else
+                            {
+                                $newJurnal->akun_debet = 0;
+                                $newJurnal->debet = 0;
+                                $newJurnal->akun_kredit = $newCoa;
+                                $newJurnal->kredit = $uraianJurnal->jumlah;
+                            }
 
-                        if($transactionSuccess)
-                        {
-                            // save as polymorphic
-                            $transactionSuccess->jurnals()->save($newJurnal);
-                        }
-                        else
-                        {
-                            $newJurnal->save();
+                            $newJurnal->keterangan = $uraianJurnal->uraian_3;
+                            
+                            if($newJurnal->keterangan == '' || $newJurnal->keterangan == null)
+                            {
+                                $newJurnal->keterangan = '-';
+                            }
+
+                            $newJurnal->created_by = 1;
+                            $newJurnal->updated_by = 1;
+                            $newJurnal->created_at = $uraianJurnal->tgl_posting;
+
+                            if($transactionSuccess)
+                            {
+                                // save as polymorphic
+                                $transactionSuccess->jurnals()->save($newJurnal);
+                            }
+                            else
+                            {
+                                $newJurnal->save();
+                            }
                         }
                     }
                 }
