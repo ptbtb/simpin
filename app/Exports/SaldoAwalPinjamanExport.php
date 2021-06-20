0349eaf73346e09678170699b1a7fcc41525e32d
Satria Persada <triasada@yahoo.com>
2021-06-20 18:56:56 +0700

    saldo awal pinjaman export

--- app/Exports/SaldoAwalPinjamanExport.php
+++ app/Exports/SaldoAwalPinjamanExport.php
@@ -13,20 +13,7 @@ class SaldoAwalPinjamanExport implements FromView, ShouldAutoSize, WithEvents
 {
     public function view(): View
     {
-        $pinjaman = Pinjaman::
-                            join('t_anggota', 't_pinjam.kode_anggota', 't_anggota.kode_anggota')
-                            ->join('t_jenis_pinjam', 't_pinjam.kode_jenis_pinjam', 't_jenis_pinjam.kode_jenis_pinjam')
-                            ->whereNotNull('saldo_mutasi')
-                            ->where('saldo_mutasi','>',0)
-                            ->select(
-                                        't_pinjam.kode_pinjam',
-                                        't_anggota.nama_anggota',
-                                        't_pinjam.kode_anggota as nomor_anggota',
-                                        't_pinjam.tgl_entri',
-                                        't_jenis_pinjam.nama_pinjaman',
-                                        't_pinjam.besar_pinjam',
-                                        't_pinjam.saldo_mutasi'
-                                    )
+        $pinjaman = Pinjaman::where('saldo_mutasi','>',0)
                             ->get();
         return view('pinjaman.exportSaldoAwalPinjaman', ['listPinjaman' => $pinjaman]);
     }
--- resources/views/pinjaman/exportSaldoAwalPinjaman.blade.php
+++ resources/views/pinjaman/exportSaldoAwalPinjaman.blade.php
@@ -4,10 +4,9 @@
         <tr>
             <th>No</th>
             <th>Kode Pinjam</th>
-            <th>Nama Anggota</th>
             <th>Nomor Anggota</th>
             <th>Tanggal Pinjaman</th>
-            <th>Jenis Pinjaman</th>
+            <th>Kode Pinjaman</th>
             <th>Besar Pinjaman</th>
             <th>Saldo Mutasi</th>
         </tr>
@@ -17,10 +16,9 @@
         <tr>
             <td>{{ $loop->iteration }}</td>
             <td>{{ $pinjaman->kode_pinjam }}</td>
-            <td>{{ $pinjaman->nama_anggota }}</td>
-            <td>{{ $pinjaman->nomor_anggota }}</td>
+            <td>{{ $pinjaman->kode_anggota }}</td>
             <td>{{ $pinjaman->tgl_entri }}</td>
-            <td>{{ $pinjaman->nama_pinjaman }}</td>
+            <td>{{ $pinjaman->kode_jenis_pinjam}}</td>
             <td>{{ $pinjaman->besar_pinjam }}</td>
             <td>{{ $pinjaman->saldo_mutasi }}</td>
         </tr>
