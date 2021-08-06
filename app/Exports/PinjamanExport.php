e6fa496026c9f587d243a054f0966eac3b48361e
Satria Persada <triasada@yahoo.com>
2021-08-06 08:46:51 +0700

    Update PinjamanExport.php

--- app/Exports/PinjamanExport.php
+++ app/Exports/PinjamanExport.php
@@ -21,27 +21,27 @@ class PinjamanExport implements FromView
         {
             $listPinjaman = $listPinjaman->where('kode_anggota', $this->request->anggota->kode_anggota);
         }
-        if (!$request->from) {
+        if (!$this->request->from) {
             $request->from = Carbon::today()->firstOfMonth()->format('Y-m-d');
           
         }
-        if (!$request->to) {
+        if (!$this->request->to) {
             $request->to = Carbon::today()->format('Y-m-d');
         }
         if ($this->request->status)
         {
             $listPinjaman = $listPinjaman->where('id_status_pinjaman', $this->request->status);
         }
-        if($request->jenistrans){
-            if($request->jenistrans=='A'){
+        if($this->request->jenistrans){
+            if($this->request->jenistrans=='A'){
                 $listPinjaman = Pinjaman::where('saldo_mutasi','>',0);
             }
-            if($request->jenistrans=='T'){
+            if($this->request->jenistrans=='T'){
                 $listPinjaman = Pinjaman::where('saldo_mutasi',0);
             }
 
         }
-        $listPinjaman = $listPinjaman->whereBetween('tgl_entri', [$request->from,$request->to]);
+        $listPinjaman = $listPinjaman->whereBetween('tgl_entri', [$this->request->from,$this->request->to]);
         $listPinjaman = $listPinjaman->get();
         return view('pinjaman.excel', [
             'listPinjaman' => $listPinjaman
