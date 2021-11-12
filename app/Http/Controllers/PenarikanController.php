2beaf783c162e7052677eda7a13cb7c5c231d3d6
musabbiharya <musabbiharya@gmail.com>
2021-11-12 20:30:30 +0700

    Update PenarikanController.php

--- app/Http/Controllers/PenarikanController.php
+++ app/Http/Controllers/PenarikanController.php
@@ -81,17 +81,51 @@ class PenarikanController extends Controller
                 return redirect()->back()->withError('Gagal melakukan penarikan. Jumlah penarikan anda tahun ini adalah '. $penarikanUser->count() .'.Maksimal penarikan dalam setahun adalah '. $simpinRule->value);
             }
 
+            $listPinjaman = Pinjaman::where('kode_anggota', $anggota->kode_anggota)
+                                    ->notPaid()
+                                    ->japan()
+                                    ->get();
+
+            $tenor1 = $listPinjaman->whereIn('lama_angsuran', [36,48,60,72])
+                                    ->values();
+            $tenor2 = $listPinjaman->whereIn('lama_angsuran', [10, 20, 30])
+                                    ->values();
+
             foreach ($request->jenis_simpanan as $kode)
             {
                 $jenissimpanan = JenisSimpanan::where('kode_jenis_simpan', $kode)->first();
                 $tabungan = $anggota->tabungan->where('kode_trans', $kode)->first();
                 $besarPenarikan = filter_var($request->besar_penarikan[$kode], FILTER_SANITIZE_NUMBER_INT);
                 $maxtarik = $tabungan->totalBesarTabungan * $jenissimpanan->max_withdraw;
-                if (is_null($tabungan)) {
+                
+                if (is_null($tabungan))
+                {
                     return redirect()->back()->withError($anggota->nama_anggota . " belum memiliki tabungan");
-                } else if ($tabungan->totalBesarTabungan < $besarPenarikan) {
+                }
+                else if ($tabungan->totalBesarTabungan < $besarPenarikan)
+                {
                     return redirect()->back()->withError("Saldo tabungan tidak mencukupi");
-                } else if ($besarPenarikan > $maxtarik + 1) {
+                }
+                else if($tenor1->count())
+                {
+                    $sisaPinjaman = $tenor1->sum('sisa_pinjaman');
+                    $minSaldo = 1/5*$sisaPinjaman;
+                    if ($tabungan->besar_tabungan < $minSaldo)
+                    {
+                        return redirect()->back()->withError("Saldo tabungan tidak mencukupi. Minimal saldo yang tersisa harus lebih dari Rp ". number_format($minSaldo, 0, ',', '.'));
+                    }
+                }
+                else if($tenor2->count())
+                {
+                    $sisaPinjaman = $tenor2->sum('sisa_pinjaman');
+                    $minSaldo = 1/8*$sisaPinjaman;
+                    if ($tabungan->besar_tabungan < $minSaldo)
+                    {
+                        return redirect()->back()->withError("Saldo tabungan tidak mencukupi. Minimal saldo yang tersisa harus lebih dari Rp ". number_format($minSaldo, 0, ',', '.'));
+                    }
+                }
+                else if ($besarPenarikan > $maxtarik + 1)
+                {
                     return redirect()->back()->withError("Penarikan simpanan " . $jenissimpanan->nama_simpanan . " tidak boleh melebihi ".$jenissimpanan->max_withdraw." dari saldo tabungan");
                 }
             }
