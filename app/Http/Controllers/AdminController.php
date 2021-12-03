3fd061ddda6fd6474a0ad64cb079250ec3861ec7
Satria Persada <triasada@yahoo.com>
2021-12-03 21:41:07 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -28,13 +28,36 @@ use Yajra\DataTables\Facades\DataTables;
 class AdminController extends Controller
 {
 	public function index(){
-		$listAnggota = Anggota::with('listSimpanan')
-                            ->whereDoesntHave('listSimpanan', function ($query)
-                            {
-                                return $query->where('periode', '2021-01-01')
-                                ->where('kode_jenis_simpan','411.12.000');
-                            })
-                            ->get();
-        dd($listAnggota);
+		// $listAnggota = Anggota::with('listSimpanan')
+  //                           ->whereDoesntHave('listSimpanan', function ($query)
+  //                           {
+  //                               return $query->where('periode', '2021-01-01')
+  //                               ->where('kode_jenis_simpan','411.12.000');
+  //                           })
+  //                           ->get();
+
+        // $simpanan = Simpanan::where('u_entry','<>','Admin BTB')
+        // ->where('mutasi',0)
+        // ->whereDoesntHave('jurnals')->get();
+        $duplicateIds = Simpanan::
+                    selectRaw("kode_anggota,kode_jenis_simpan,periode,min(kode_simpan) as kode_simpan ")
+                    ->wherein('kode_jenis_simpan',['411.12.000','502.01.000'])
+                    ->whereraw("periode>'2021-10-01'")
+                    ->groupBy("kode_anggota","kode_jenis_simpan","periode")
+                    ->havingRaw('count(periode) > ?', [1])
+                    // ->toSql();
+                     ->pluck("kode_simpan");
+                   
+       // dd($duplicateIds);
+       
+        foreach ($duplicateIds as $id){
+            $simpanannya = Simpanan::find($id);
+            if ($simpanannya->jurnals->count()>0){
+                $simpanannya->jurnals[0]->delete();
+            }
+            $simpanannya->delete();
+             // dd($id);
+
+        }
 	}
 }
