3334378b22b41d88b6d5117c1edbd8728488b818
Satria Persada <triasada@yahoo.com>
2021-12-03 14:31:49 +0700

    adminscript

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Exports\KartuSimpananExport;
+use App\Exports\LaporanExcelExport;
+use App\Exports\SimpananExport;
+use App\Imports\SimpananImport;
+use App\Models\Anggota;
+use App\Models\JenisSimpanan;
+use App\Models\Jurnal;
+use App\Models\Penarikan;
+use App\Models\Simpanan;
+use App\Models\Code;
+use Illuminate\Http\Request;
+
+use App\Managers\JurnalManager;
+use App\Managers\SimpananManager;
+use App\Models\Company;
+use Auth;
+use DB;
+use Hash;
+use Carbon\Carbon;
+use Excel;
+use Illuminate\Support\Facades\Log;
+use PDF;
+use Yajra\DataTables\Facades\DataTables;
+
+class AdminController extends Controller
+{
+	public function index(){
+		$listAnggota = Anggota::with('listSimpanan')
+                            ->whereDoesntHave('listSimpanan', function ($query)
+                            {
+                                return $query->where('periode', '2021-01-01');
+                            })
+                            ->get();
+        dd($listAnggota[0]->listSimpanan);
+	}
+}
