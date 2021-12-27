2c7328deba26239d15172d3f3199c0d39b0df9e4
musabbiharya <musabbiharya@gmail.com>
2021-12-27 16:52:15 +0800

    Create UpdatePinjamanTopup.php

---
+++ app/Console/Commands/UpdatePinjamanTopup.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\PengajuanTopup;
+use Illuminate\Console\Command;
+
+class UpdatePinjamanTopup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'topuppinjaman:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update topup pinjaman';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $pengajuanTopup = PengajuanTopup::whereNull('total_bayar_pelunasan_dipercepat')
+                                        ->get();
+        $pengajuanTopup->each(function ($topup)
+        {
+            $pinjaman = $topup->pinjaman;
+            $topup->jasa_pelunasan_dipercepat = $pinjaman->jasaTopup;
+            $topup->total_bayar_pelunasan_dipercepat = $pinjaman->totalBayarTopup;
+            $topup->save();
+
+            echo "topup id ".$topup->id." updated\n";
+        });
+    }
+}
