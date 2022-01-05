8aa4f85895435e2339a6334f4bd164a61de8b1d1
Satria Persada <triasada@yahoo.com>
2022-01-05 13:36:37 +0700

    Update UpdatePinjamanTopup.php

--- app/Console/Commands/UpdatePinjamanTopup.php
+++ app/Console/Commands/UpdatePinjamanTopup.php
@@ -44,12 +44,15 @@ class UpdatePinjamanTopup extends Command
                                         ->get();
         $pengajuanTopup->each(function ($topup)
         {
-            $pinjaman = $topup->pinjaman;
+            if($topup->pinjaman){
+              $pinjaman = $topup->pinjaman;
             $topup->jasa_pelunasan_dipercepat = $pinjaman->jasaTopup;
             $topup->total_bayar_pelunasan_dipercepat = $pinjaman->totalBayarTopup;
             $topup->save();
 
-            echo "topup id ".$topup->id." updated\n";
+            echo "topup id ".$topup->id." updated\n";  
+            }
+            
         });
     }
 }
