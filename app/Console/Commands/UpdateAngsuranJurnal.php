88d63df57e384505fb643affa38a27af2be2eabc
Satria Persada <triasada@yahoo.com>
2022-01-06 23:56:25 +0700

    Create UpdateAngsuranJurnal.php

---
+++ app/Console/Commands/UpdateAngsuranJurnal.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Console\Commands;
+
+Use App\Models\Angsuran;
+Use App\Models\Jurnal;
+use App\Managers\AngsuranPartialManager;
+use Illuminate\Console\Command;
+use DB;
+
+class UpdateAngsuranJurnal extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:angsuranjurnal';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update Angsuran';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $listAngsuran = DB::table('t_jurnal_backup_060121')
+                        ->where('kredit','>',0)
+                        ->where('jurnalable_type','App\\Models\\Angsuran')
+                        ->get();
+
+        foreach($listAngsuran as $angs){
+
+        DB::beginTransaction();  
+        try
+        {
+            $jurnal = Jurnal::find($angs->id);
+            $jurnal->akun_debet = $angs->akun_kredit;
+            $jurnal->debet = $angs->kredit;
+            $jurnal->akun_kredit = $angs->akun_debet;
+            $jurnal->kredit = $angs->debet;
+            $jurnal->save();
+            DB::commit();
+        }
+        catch(\Exception $e)
+        {
+        DB::rollback();
+        \Log::info($e->getMessage());
+        throw new \Exception($e->getMessage());
+        }
+            
+            echo $angs->id."\n";
+        }
+         
+    
+    }
+}
