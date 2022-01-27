333914f11d9967f781dc26dedc712d09306c96f4
Satria Persada <triasada@yahoo.com>
2022-01-27 23:12:46 +0800

    Update viewjurnal.blade.php

--- resources/views/pinjaman/viewjurnal.blade.php
+++ resources/views/pinjaman/viewjurnal.blade.php
@@ -15,8 +15,39 @@
                         <td>{{ $pengajuan->kode_jenis_pinjam }}</td>
                         <td>Rp. {{ number_format($pengajuan->besar_pinjam, 2, ',', '.') }}</td>
                         <td>Bank/Kas</td>
-                        <td>{{ $pengajuan->view_credit_bank }}</td>
+                        <td>{{ number_format($pengajuan->pinjaman->pinjamanDitransfer,2,',','.') }}</td>
                     </tr>
+                    @if ($pengajuan->pengajuanTopup->count())
+                      <tr>
+                          <td></td>
+                          <td></td>
+                          @php
+                            $jenis = substr($pengajuan->pengajuanTopup[0]->pinjaman->kode_jenis_pinjam,0,3);
+                            if ($jenis==106){
+                              $jasatopup='701.02.014';
+                            }elseif($jenis==105){
+                              $jasatopup='701.02.015';
+                            }
+
+                          @endphp
+                            <td>{{$jasatopup}}</td>
+                            <td>{{ $pengajuan->viewJasaPelunasanDipercepat }}</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td>{{ $pengajuan->pengajuanTopup[0]->pinjaman->kode_jenis_pinjam}}</td>
+                            <td>{{ $pengajuan->viewSisaPinjaman }}</td>
+                        </tr>
+                    @endif
+                    @if ($pengajuan->transfer_simpanan_pagu)
+                      <tr>
+                          <td></td>
+                          <td></td>
+                            <td>Simpanan Pagu</td>
+                            <td>Rp {{ number_format($pengajuan->transfer_simpanan_pagu, '2', ',', '.') }}</td>
+                        </tr>
+                    @endif
                     <tr>
                         <td></td>
                         <td></td>
