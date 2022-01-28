e08b336c955306cb8e7994ed04c599fef904a09c
Satria Persada <triasada@yahoo.com>
2022-01-28 20:31:19 +0800

    Update TransaksiController.php

--- app/Http/Controllers/api/TransaksiController.php
+++ app/Http/Controllers/api/TransaksiController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Anggota;
 use App\Models\Simpanan;
 use App\Models\Penarikan;
+use App\ModelsView\ViewTransaksiSimpan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
