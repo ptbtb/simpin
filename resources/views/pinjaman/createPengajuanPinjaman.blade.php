f85de6559db06365d84686502ead59e0abc2e04e
Satria Persada <triasada@yahoo.com>
2022-08-01 15:20:24 +0700

    Update createPengajuanPinjaman.blade.php

--- resources/views/pinjaman/createPengajuanPinjaman.blade.php
+++ resources/views/pinjaman/createPengajuanPinjaman.blade.php
@@ -244,12 +244,12 @@
             @endif
             initialEvent();
 
-            // @if (\Auth::user()->isAdmin())
+            @if (!\Auth::user()->isAnggota())
                 $('#tanggal_pengajuan').datepicker({
                     uiLibrary: 'bootstrap4',
                     format: 'yyyy-mm-dd'
                 });
-            // @endif
+            @endif
         });
 
         function initialEvent() {
