6e57a0a2a8538ddc6993cd28d716dc99fc567f77
Satria Persada <triasada@yahoo.com>
2022-08-10 10:43:57 +0700

    Update SimpananController.php

--- app/Http/Controllers/SimpananController.php
+++ app/Http/Controllers/SimpananController.php
@@ -187,7 +187,7 @@ class SimpananController extends Controller
                     $simpanan->u_entry = Auth::user()->name;
                     $simpanan->tgl_entri = Carbon::now();
                     $simpanan->tgl_transaksi = Carbon::createFromFormat('d-m-Y', $request->tgl_transaksi[$key]);
-                    $simpanan->periode = Carbon::createFromFormat('d-m-Y', $request->tgl_transaksi[$key]);
+                    $simpanan->periode = Carbon::createFromFormat('d-m-Y', $request->periode[$key]);
                     $simpanan->kode_jenis_simpan = $jenisSimpanan->kode_jenis_simpan;
                     $simpanan->keterangan = ($request->keterangan[$key]) ? $request->keterangan[$key] : '';
                     $simpanan->id_akun_debet = ($request->id_akun_debet[$key]) ? $request->id_akun_debet[$key] : null;
