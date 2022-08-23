19360b155658150f2581d4e2bd61ccc4b4a5b529
Satria Persada <triasada@yahoo.com>
2022-08-23 19:42:13 +0700

    update

--- resources/views/neraca/excel.blade.php
+++ resources/views/neraca/excel.blade.php
@@ -12,7 +12,7 @@
                          <th colspan="5" style="text-align: center; font-weight: bold;"> Neraca Periode {{$request->period}}</th>
                        </tr>
                         <tr>
-                            <th></th>
+                            <th>Jenis</th>
                             <th>Rek</th>
                             <th>Nama Rekening</th>
                             <th >Bulan ini</th>
@@ -20,142 +20,20 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                                <td colspan="5">AKTIVA</td>
-                             </tr>
-                        <tr>
 
-                            <td>I</td>
-                            <td>AKTIVA LANCAR</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        @foreach ($aktivalancar as $item)
+                        @foreach ($list as $item)
                             <tr>
-                                <td></td>
-                                <td>{{ substr($item['code']->CODE, 0, 3) }}</td>
-                                <td>{{ $item['code']->NAMA_TRANSAKSI }}</td>
+                                <td>{{ $item['Kategori'] }}</td>
+                                <td>{{ $item['CODE'] }}</td>
+                                <td>{{ $item['NAMA_TRANSAKSI'] }}</td>
                                 <td>{{$item['saldo'] }}</td>
-                                <td>{{$item['saldoLastMonth'] }}</td>
+                                <td>{{$item['saldoLalu'] }}</td>
                             </tr>
                         @endforeach
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td>Jumlah Aktiva Lancar</td>
-                            <td>{{$aktivalancar->sum('saldo') }}</td>
-                            <td>{{$aktivalancar->sum('saldoLastMonth') }}</td>
-                        </tr>
-                        <tr>
-                            <td>II</td>
-                            <td>AKTIVA TETAP</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        @foreach ($aktivatetap as $item)
-                            <tr>
-                                <td></td>
-                                <td>{{ substr($item['code']->CODE, 0, 3) }}</td>
-                                <td>{{ $item['code']->NAMA_TRANSAKSI }}</td>
-                                <td>{{$item['saldo'] }}</td>
-                                <td>{{$item['saldoLastMonth'] }}</td>
-                            </tr>
-                        @endforeach
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td>Jumlah Aktiva Tetap</td>
-                            <td>{{$aktivatetap->sum('saldo') }}</td>
-                            <td>{{$aktivatetap->sum('saldoLastMonth') }}</td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td>TOTAL AKTIVA</td>
-                            <td>{{$aktivatetap->sum('saldo')+$aktivalancar->sum('saldo') }}</td>
-                            <td>{{$aktivatetap->sum('saldoLastMonth')+$aktivalancar->sum('saldoLastMonth') }}</td>
-                        </tr>
-                            <tr>
-                                <td colspan="5">PASIVA</td>
-                             </tr>
-                        <tr>
-                            <td>III</td>
-                            <td>KEWAJIBAN LANCAR</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        @foreach ($kewajibanlancar as $item)
-                            <tr>
-                                <td></td>
-                                <td>{{ substr($item['code']->CODE, 0, 3) }}</td>
-                                <td>{{ $item['code']->NAMA_TRANSAKSI }}</td>
-                                <td>{{$item['saldo'] }}</td>
-                                <td>{{$item['saldoLastMonth'] }}</td>
-                            </tr>
-                        @endforeach
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td>Jumlah Kewajiban Lancar</td>
-                            <td>{{$kewajibanlancar->sum('saldo') }}</td>
-                            <td>{{$kewajibanlancar->sum('saldoLastMonth') }}</td>
-                        </tr>
-                        <tr>
-                            <td>IV</td>
-                            <td>KEWAJIBAN JANGKA PANJANG</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        @foreach ($kewajibanjangkapanjang as $item)
-                            <tr>
-                                <td></td>
-                                <td>{{ substr($item['code']->CODE, 0, 3) }}</td>
-                                <td>{{ $item['code']->NAMA_TRANSAKSI }}</td>
-                                <td>{{$item['saldo'] }}</td>
-                                <td>{{$item['saldoLastMonth'] }}</td>
-                            </tr>
-                        @endforeach
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td>Jumlah Kewajiban Jangka Panjang</td>
-                            <td>{{$kewajibanjangkapanjang->sum('saldo') }}</td>
-                            <td>{{$kewajibanjangkapanjang->sum('saldoLastMonth') }}</td>
-                        </tr>
-                        <tr>
-                            <td>V</td>
-                            <td>KEKAYAAN BERSIH</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        @foreach ($kekayaanbersih as $item)
-                            <tr>
-                                <td></td>
-                                <td>{{ substr($item['code']->CODE, 0, 3) }}</td>
-                                <td>{{ $item['code']->NAMA_TRANSAKSI }}</td>
-                                <td>{{$item['saldo'] }}</td>
-                                <td>{{$item['saldoLastMonth'] }}</td>
-                            </tr>
-                        @endforeach
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td>Jumlah Kekayaan Bersih</td>
-                            <td>{{$kekayaanbersih->sum('saldo') }}</td>
-                            <td>{{$kekayaanbersih->sum('saldoLastMonth') }}</td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td>TOTAL PASIVA</td>
-                            <td>{{$kekayaanbersih->sum('saldo')+$kewajibanjangkapanjang->sum('saldo')+$kewajibanlancar->sum('saldo') }}</td>
-                            <td>{{$kekayaanbersih->sum('saldoLastMonth')+$kewajibanjangkapanjang->sum('saldoLastMonth')+$kewajibanlancar->sum('saldoLastMonth') }}</td>
-                        </tr>
+
+
+
+
                     </tbody>
                     <tfoot>
                       <tr>
