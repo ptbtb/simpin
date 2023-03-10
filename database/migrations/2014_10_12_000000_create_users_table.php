85a6523d93147414253b6db70df16f690b38f728
aini <ainie1224@gmail.com>
2023-03-10 16:01:57 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,20 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->unsignedBigInteger('id')->autoIncrement();
+            $table->unsignedBigInteger('kode_anggota')->nullable();
+            $table->string('name', 190);
+            $table->string('email', 190)->unique();
+            $table->string('password', 190);
             $table->rememberToken();
+            $table->string('photo_profile_path', 100)->nullable();
+            $table->string('activation_code', 190)->nullable();
+            $table->tinyInteger('is_verified')->default(0);
+            $table->dateTime('email_verified_at')->nullable();
+            $table->integer('created_by')->nullable();
+            $table->integer('is_allow')->default(1)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
